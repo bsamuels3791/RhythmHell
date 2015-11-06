f37e4f5ca931a5495d49aa23f304f29ec08ea20d
Ben Samuels <bsamuels3791@yahoo.com>
2015-11-06 17:47:00 -0500

    *Added basic UI for start menu

---
+++ RhythmHell/Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenuManager : MonoBehaviour {
+
+    public Button startButton;
+    public Button helpButton;
+    public Button backButton;
+    public Text helpText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /***
+     * Event triggered when 'Start' button is clicked
+     * Loads Level 1 of the current build
+    ***/
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    /***
+     * Event triggered when 'Help' button is clicked
+     * Hides the 'Start' and 'Help' buttons
+     * Shows Help text and 'Back' button
+    ***/
+    public void ShowHelp()
+    {
+        // Hide start and help buttons
+        startButton.gameObject.SetActive(false);
+        helpButton.gameObject.SetActive(false);
+        // Show help text and back button
+        helpText.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(true);
+    }
+
+    /***
+     * Event triggered when 'Back' button is clicked
+     * Hides the Help text and 'Back' button
+     * Shows 'Help' and 'Start' buttons
+    ***/
+    public void HideHelp()
+    {
+        // Show start and help buttons
+        startButton.gameObject.SetActive(true);
+        helpButton.gameObject.SetActive(true);
+        // Hide help text and back button
+        helpText.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+    }
+}
