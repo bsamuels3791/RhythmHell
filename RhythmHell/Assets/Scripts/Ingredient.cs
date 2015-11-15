c080444264f2f8b2ec9ea982af72c4c3d52f878c
Ian <inhampson@gmail.com>
2015-11-15 16:55:32 -0500

    adding a basic Ingredient class.

---
+++ RhythmHell/Assets/Scripts/Ingredient.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Ingredient : MonoBehaviour {
+  public type;
+
+  void Start(){
+
+  }
+
+  void AddThis(){
+    
+  }
+
+}
