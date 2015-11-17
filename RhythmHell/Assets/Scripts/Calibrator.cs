10c478f5dbe58156084f4d3135c1ed53969583f8
Ben Samuels <bsamuels3791@yahoo.com>
2015-11-17 16:01:47 -0500

    *Calibration screen now loads into game level *Calibration visuals are blocked out, like in the Rhythm Heaven calibration, after 2 samples

--- RhythmHell/Assets/Scripts/Calibrator.cs
+++ RhythmHell/Assets/Scripts/Calibrator.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class Calibrator : RhythmObject
 {
     private const int MAX_SAMPLES = 8;
+    // How many samples to take before we hide the visuals from the user, making them rely on sound
+    private const int HIDE_BUTTONS_AT = 2;
 
     private RhythmAnalyzer analyzer;
     private bool calibrating;
+
+    private GameObject hideButtonSprite;
+    private Text noticeText;
 
     // Use this for initialization
     void Start()
     {
         analyzer = new RhythmAnalyzer();
         calibrating = true;
+
+        hideButtonSprite = GameObject.Find("HideButtons");
+        noticeText = GameObject.Find("Notice").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -27,6 +35,11 @@ public class Calibrator : RhythmObject
             // Still collecting calibration samples
             if (samples.Count < MAX_SAMPLES)
             {
+                // Check if we need to hide the sampling visuals
+                if (samples.Count == HIDE_BUTTONS_AT)
+                {
+                    hideButtonSprite.GetComponent<SpriteRenderer>().enabled = true;
+                }
                 // Check for Spacebar press
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
@@ -34,7 +47,10 @@ public class Calibrator : RhythmObject
                     analyzer.AddSample(sample);
                 }
 
-                GameObject.Find("Notice").GetComponent<Text>().text =
+               /* GameObject.Find("Notice").GetComponent<Text>().text =
+                    "Samples collected:\n" +
+                    samples.Count + " / " + MAX_SAMPLES;*/
+                noticeText.text =
                     "Samples collected:\n" +
                     samples.Count + " / " + MAX_SAMPLES;
             }
@@ -44,8 +60,10 @@ public class Calibrator : RhythmObject
             {
                 float offset = analyzer.GetAverageOffset();
 
-                GameObject.Find("Notice").GetComponent<Text>().text =
-                    "Global Offset:\n" + offset;
+                /*GameObject.Find("Notice").GetComponent<Text>().text =
+                    "Global Offset:\n" + offset;*/
+                noticeText.text =
+                    "Global Offset: \n" + offset;
 
                 calibrating = false;
                 beatMachine.Stop();
@@ -55,13 +73,21 @@ public class Calibrator : RhythmObject
         }
         else
         {
-            GameObject.Find("Notice").GetComponent<Text>().text =
-                "Press Space to move to the game!";
+            /*GameObject.Find("Notice").GetComponent<Text>().text =
+                "Press Space to move to the game!\n\n Or, press 'R' to re-do the calibration";*/
+            noticeText.text =
+                "Press Space to move to the game!\n\n Or, press 'R' to re-do the calibration";
 
             // Check for Spacebar press
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Application.LoadLevel(3);
+                Application.LoadLevel(2);
+            }
+
+            // Check for 'R' release
+            if (Input.GetKeyUp(KeyCode.R))
+            {
+                Application.LoadLevel(1);
             }
         }
     }
