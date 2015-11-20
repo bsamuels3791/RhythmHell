3684088ef98ea23c38a26e70fd1809017d9e6c99
unknown <imh8602@GDD-54.main.ad.rit.edu>
2015-11-20 13:44:51 -0500

    Merged Ticket.cs, changes Sprite with type.

--- RhythmHell/Assets/Scripts/Ticket.cs
+++ RhythmHell/Assets/Scripts/Ticket.cs
@@ -8,13 +8,46 @@ public enum TicketType{
 }
 
 public class Ticket : MonoBehaviour {
-	//public string type = "Cheese";  // If no type is specified, default to cheese
 	// Sprite for Cheese, Sausage, and Veggie tickets
 	public Sprite cheeseTicket;
 	public Sprite sausageTicket;
 	public Sprite veggieTicket;
+	public string type = "Cheese";  // If no type is specified, default to cheese
+	private TicketType ticketObj;
 
-	public TicketType ticketObj;
+	// Use this for initialization
+	void Start () {
+		setType(type);
+	}
+
+	void changeType(string newType = default(string)){
+		if(newType != null){
+			// Changing type
+			setType(newType);
+		} else{
+			Debug.Log("no type passed into changeType");
+		}
+	}
+
+	void setType(string newType){
+		switch (newType) {
+		case "Cheese":
+			ticketObj = TicketType.CHEESE;
+			gameObject.GetComponent<SpriteRenderer> ().sprite = cheeseTicket;
+			//SetSprite("ticket_cheese");
+			break;
+		case "Sausage":
+			ticketObj = TicketType.SAUSAGE;
+			gameObject.GetComponent<SpriteRenderer> ().sprite = sausageTicket;
+			//SetSprite("ticket_sausage");
+			break;
+		case "Veggie":
+			ticketObj = TicketType.VEGGIE;
+			gameObject.GetComponent<SpriteRenderer> ().sprite = veggieTicket;
+			//SetSprite("ticket_veggie");
+			break;
+		}	
+	}
 
 	public TicketType GetTicketType(){ 
 		switch (ticketObj) {
@@ -22,15 +55,12 @@ public class Ticket : MonoBehaviour {
 		default:
 		case TicketType.CHEESE:
 			ticketObj = TicketType.CHEESE;
-			gameObject.GetComponent<SpriteRenderer> ().sprite = cheeseTicket;
 			break;
 		case TicketType.SAUSAGE:
 			ticketObj = TicketType.SAUSAGE;
-			gameObject.GetComponent<SpriteRenderer> ().sprite = sausageTicket;
 			break;
 		case TicketType.VEGGIE:
 			ticketObj = TicketType.VEGGIE;
-			gameObject.GetComponent<SpriteRenderer> ().sprite = veggieTicket;
 			break;
 		}
 
